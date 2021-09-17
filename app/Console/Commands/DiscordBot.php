0c0b76411f0c4c175da0ad2e3fc6dced3e7492a1
Kavin Valli <kavinvalli@gmail.com>
2021-09-17 23:40:44 +0530

    feat: Discord and User lookup

--- app/Console/Commands/DiscordBot.php
+++ app/Console/Commands/DiscordBot.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use App\Events\NotificationCreated;
 use App\Models\Notification;
+use App\Models\User;
 use Discord\Discord;
+use Discord\Parts\Embed\Embed;
 use Illuminate\Console\Command;
 
 class DiscordBot extends Command
@@ -33,6 +35,83 @@ class DiscordBot extends Command
     parent::__construct();
   }
 
+  private function decodeUsername($content)
+  {
+    $splitUp = explode(" ", $content);
+    array_shift($splitUp);
+    $a = explode("#", $splitUp[count($splitUp) - 1]);
+    $discriminator = array_pop($a);
+    $username = $a[0];
+    echo var_dump([$username, $discriminator]);
+    return [$username, $discriminator];
+  }
+
+  private function createNotification($content, $image)
+  {
+    $notif = new Notification();
+    $notif->content = $content . $image ? " <img class=\"w-80\" src=\"{$image}\"></img>" : "";
+    $notif->save();
+
+    broadcast(new NotificationCreated());
+  }
+
+  private function sendUserEmbed($message, $user, $discord)
+  {
+    $embed = new Embed($discord);
+    $embed->setTitle($user->username);
+    $embed->addFieldValues('Name', $user->name, false);
+    $embed->addFieldValues('Email', $user->email, false);
+    $embed->addFieldValues('Discord', $user->discord_id . $user->discord_discriminator, false);
+    $embed->addFieldValues('Institution', $user->institution, false);
+    $embed->addFieldValues('Circle', $user->circle, false);
+    $embed->addFieldValues('Level', $user->level, false);
+    $embed->addFieldValues('Points', $user->points, false);
+
+    $message->channel->sendEmbed($embed);
+  }
+
+  private function discordLookup($message, $discord)
+  {
+    $data = $this->decodeUsername($message->content);
+    $username = $data[0];
+    $discriminator = $data[1];
+
+    echo $username . " " . $discriminator;
+
+    $user = User::select('name', 'username', 'email', 'institution', 'circle', 'level', 'points', 'discord_id', 'discord_discriminator')
+      ->where('discord_username', $username)
+      ->where('discord_discriminator', $discriminator)
+      ->get()[0];
+
+    if (!$user) {
+      $message->channel->sendMessage("User with username " . $username . "and discriminator " . $discriminator . " not found");
+      return;
+    }
+
+    $this->sendUserEmbed($message, $user, $discord);
+  }
+
+  private function userLookup($message, $discord)
+  {
+    /* $data = $this->decodeUsername($message->content); */
+    /* $username = $data[0]; */
+    /* $discriminator = $data[1]; */
+    $splitUp = explode(" ", $message->content);
+    array_shift($splitUp);
+    $username = $splitUp[0];
+
+    $user = User::select('name', 'username', 'email', 'institution', 'circle', 'level', 'points', 'discord_username', 'discord_discriminator')
+      ->where('username', $username)
+      ->get()[0];
+
+    if (!$user) {
+      $message->channel->sendMessage("User with username " . $username . " not found");
+      return;
+    }
+
+    $this->sendUserEmbed($message, $user, $discord);
+  }
+
   /**
    * Execute the console command.
    *
@@ -49,19 +128,19 @@ class DiscordBot extends Command
 
       // Listen for messages.
       $discord->on('message', function ($message, $discord) {
-        echo "{$message->author->username}: {$message->content}: {$message->channel_id}", PHP_EOL;
-        echo var_dump($message->attachments);
+        /* echo "{$message->author->username}: {$message->content}: {$message->channel_id}", PHP_EOL; */
+        /* echo var_dump($message->attachments); */
 
         if ($message->channel_id === env("DISCORD_HINTS_CHANNEL_ID")) {
-          $notif = new Notification();
-          if (count($message->attachments) > 0) {
-            $notif->content = $message->content . " <img class=\"w-80\" src=\"{$message->attachments[0]->url}\"></img>";
-          } else {
-            $notif->content = $message->content;
-          }
-          $notif->save();
+          $this->createNotification($message->content, count($message->attachments) > 0 ? $message->attachments[0]->url : "");
+        }
 
-          broadcast(new NotificationCreated());
+        if (str_starts_with($message->content, "!dlookup")) {
+          $this->discordLookup($message, $discord);
+        }
+
+        if (str_starts_with($message->content, "!ulookup")) {
+          $this->userLookup($message, $discord);
         }
       });
     });
