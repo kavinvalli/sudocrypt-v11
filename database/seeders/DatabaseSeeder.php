2e7c40c55c781b089e4e81caa32205a276ac6f11
Kavin Valli <kavinvalli@gmail.com>
2021-09-25 20:40:53 +0530

    fix: Add LevelSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     $this->call([
       UserSeeder::class,
       CircleSeeder::class,
+      LevelSeeder::class,
     ]);
     // \App\Models\User::factory(10)->create();
   }
