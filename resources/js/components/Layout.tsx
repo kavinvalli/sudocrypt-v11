e3617a6edd788542a40a1eb4b02757d8ce380c45
Angad Singh <mail@angad.dev>
2021-10-07 11:57:10 +0530

    ui: move Layout to page instead of app

--- resources/js/components/Layout.tsx
+++ resources/js/components/Layout.tsx
@@ -2,19 +2,34 @@ import React from "react";
 
 interface IProps {
   children?: JSX.Element;
+  footer?: boolean;
 }
 
-const Layout: React.FC<IProps> = ({ children }: IProps) => {
+const Layout: React.FC<IProps> = ({ children, footer }: IProps) => {
   return (
     <div
-      className="bg-dark min-h-screen text-white"
+      className="bg-dark w-full min-h-screen text-white flex flex-col"
       style={{
         background:
           "repeating-radial-gradient(circle farthest-corner at 0% 100%, #e5543b, #e5543b 1px, #161a1d 1px, #161a1d 100px)",
         backgroundPositionX: "150%",
       }}
     >
-      {children}
+      <div className="flex-1 flex items-center">
+        {children}
+      </div>
+      {footer && (
+        <div className="flex flex-col justify-center items-center py-5">
+          <div className="w-full flex justify-center items-center mb-3 gap-x-2">
+            <div className="w-2 h-2 rounded-full bg-[#535353]"></div>
+            <div className="w-2 h-2 rounded-full bg-[#535353]"></div>
+            <div className="w-2 h-2 rounded-full bg-[#535353]"></div>
+          </div>
+          <div className="text-gray-400 font-bold text-sm">
+            &copy; Exun Clan 2021
+          </div>
+        </div>
+      )}
     </div>
   );
 };
