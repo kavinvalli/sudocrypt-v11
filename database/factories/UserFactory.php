e986418dfec7f680abffad4173c47920e5db461a
Angad Singh <mail@angad.dev>
2021-11-17 13:48:45 +0530

    chore: setup UserFactory, UserSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,14 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'username' => $this->faker->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'institution' => $this->faker->company(),
+            'circle_id' => 1,
+            'level_id' => 1
         ];
     }
 
@@ -41,6 +45,20 @@ class UserFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the model should be an admin.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'admin' => true,
             ];
         });
     }
