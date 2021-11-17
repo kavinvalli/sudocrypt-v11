c03d200127363478320489e2913244bcf76e5a2a
Angad Singh <mail@angad.dev>
2021-11-17 21:46:36 +0530

    refactor: add ts types for backend models

--- resources/js/lib/types.tsx
+++ resources/js/lib/types.tsx
@@ -1,4 +1,4 @@
-import { Page, PageProps } from "@inertiajs/inertia";
+import { ErrorBag, Errors, Page, PageProps } from "@inertiajs/inertia";
 
 export interface IUser {
   id: number;
@@ -7,23 +7,84 @@ export interface IUser {
   email: string;
   name: string;
   email_verified_at?: string;
-  level: number;
   admin: boolean | number;
   points: number;
+  institution: string;
+  disqualified: boolean | number;
+  last_solved: string;
+  level_id?: number;
+  circle_id: number;
+
+  discord_id?: string;
+  discord_username?: string;
+  discord_discriminator?: string;
+  discord_email?: string;
+
+  level?: ILevel;
+  circle?: ICircle;
+}
+
+export interface ILevel {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  circle_id: string;
+  question: string;
+  source_hint?: string;
+  answer?: string;
+  points: number;
+
+  circle?: ICircle;
+}
+
+export interface ICircle {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  name: string;
+
+  levels?: ILevel[];
+}
+
+export interface IUserAttempt {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  level_id: number;
+  circle_id: number;
+  user_id: number;
+  attempt: string;
+  ip: string;
+  correct: boolean;
+
+  level?: ILevel;
+  circle?: ICircle;
+  user?: IUser;
+}
+
+export interface IShortlink {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  shortlink: string;
+  url: string;
+}
+
+export interface INotification {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  content: string;
 }
 
 export interface IPageProps extends Page<PageProps> {
   props: {
-    circle: {
-      name: string;
-      id: number;
-      onlyOneLevel: boolean;
-    };
+    circle: ICircle;
+    errors: Errors & ErrorBag;
     dates: {
       start: string;
       end: string;
     };
-    errors: any;
     started: boolean;
     ended: boolean;
     authenticated: boolean;
