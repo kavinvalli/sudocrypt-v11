7e6d12f63ab75d1134722cd00a4de4b8a1f66983
Angad Singh <mail@angad.dev>
2021-11-22 05:58:58 +0530

    ui: admin index page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
+use App\Models\Shortlink;
 use App\Models\User;
 use App\Models\UserAttempt;
 use Illuminate\Http\Request;
@@ -12,9 +14,12 @@ class AdminController extends Controller
   public function show()
   {
     return Inertia::render('admin/index', [
-      'users' => User::count(),
+      'users' => User::where('admin', false)->count(),
       'discord_accounts' => User::whereNotNull('discord_id')->count(),
       'attempts' => UserAttempt::count(),
+      'levels_solved' => UserAttempt::where('correct', true)->count(),
+      'notifications' => Notification::count(),
+      'shortlinks' => Shortlink::count()
     ]);
   }
 }
