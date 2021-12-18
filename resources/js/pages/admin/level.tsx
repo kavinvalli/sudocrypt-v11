0695d583f0646e3d60ea42c3394ec6843ef0f0b5
Angad Singh <mail@angad.dev>
2021-12-18 21:44:00 +0530

    ui: level page fix width

--- resources/js/pages/admin/level.tsx
+++ resources/js/pages/admin/level.tsx
@@ -74,8 +74,8 @@ const Level: React.FC<IAdminLevelProps> = ({
             </div>
           ))}
         </div>
-        <div>
-          <div className="bg-dark-lighter px-6 py-2 rounded-lg max-w-sm sm:max-h-screen sm:overflow-y-auto w-full">
+        <div className="w-full sm:max-w-sm px-5 py-10 sm:p-0">
+          <div className="bg-dark-lighter px-6 py-2 rounded-lg sm:max-h-screen sm:overflow-y-auto w-full">
             <div className="my-5 border-b border-gray-600">
               {[
                 ["Attempts", level?.attempts_count],
