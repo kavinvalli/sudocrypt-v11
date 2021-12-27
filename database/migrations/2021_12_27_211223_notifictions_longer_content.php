3b63900ef920938c2b36abea4360b3c8d39844c9
Kavin Valli <kavinvalli@gmail.com>
2021-12-27 21:16:00 +0530

    fix: make notification content accept larger data

---
+++ database/migrations/2021_12_27_211223_notifictions_longer_content.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class NotifictionsLongerContent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->mediumText('content')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->string('content')->change();
+        });
+    }
+}
